8f6750716fcb6ab564c4df422b4ace6cf76b78f4
vikramoff24 <vikram.senthil@outlook.com>
2020-09-14 09:58:59 +0530

    Initial commit

---
+++ bootjpa/src/main/java/com/example/demo/model/Aliens.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+public class Aliens {
+	private int aid;
+	private String aname;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	@Override
+	public String toString() {
+		return "Aliens [aid=" + aid + ", aname=" + aname + "]";
+	}
+	}
